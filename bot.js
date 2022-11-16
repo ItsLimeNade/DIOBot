932f74919a5cf5b8d5fd386989da141ab6851b31
LimeNade <alexandre.zarkua@gmail.com>
2022-11-16 10:01:28 +0100

    Finished leveling

--- bot.js
+++ bot.js
@@ -7,6 +7,7 @@
     const events = require('events');
     const { exec } = require("child_process")
     const Discord = require("discord.js")
+    const Leveling = require('./leveling')
     const {
         MessageEmbed,
         MessageButton,
@@ -54,6 +55,9 @@
     // })
 
     // code
+
+    let leveling = new Leveling(0, 100, 1, 1, 1, 1.2) //Params in order: startingXP: any, neededXP: any, gainedXP: any, xpRate: any, startingLevel: any, levelingRate: any
+
     await s4d.client.login(process.env.TOKEN)
 
     synchronizeSlashCommands(s4d.client, [{
@@ -62,10 +66,22 @@
         options: [
 
         ]
+    }, {
+        name: 'level',
+        description: 'Shows you your current level',
+        options: [{
+            type: 6,
+            name: 'member',
+            required: true,
+            description: 'see the level of an other user',
+            choices: [
+
+            ]
+        },]
     },], {
         debug: false,
 
-    })
+    });
 
     s4d.client.on('interactionCreate', async (interaction) => {
         let member = interaction.guild.members.cache.get(interaction.member.user.id)
@@ -76,23 +92,45 @@
                 components: []
             })
         }
+        if ((interaction.commandName) == 'level') {
+            slashMember = (interaction.options.getMember('member'));
+            await leveling.checkData(slashMember.id, interaction.guild.id)
+            const userData = await leveling.getUserData(slashMember.id, interaction.guild.id)
+            const rank = new canvaCord.Rank()
+                .setAvatar(slashMember.displayAvatarURL({ format: "png" }))
+                .setCurrentXP(await userData.currentXP)
+                .setRequiredXP(await userData.neededXP)
+                .setStatus("dnd")
+                .setProgressBar("#FFFFFF", "COLOR")
+                .setUsername("Snowflake")
+                .setDiscriminator("0007")
+                .setLevel(await userData.currentLevel, 'Level', true)
+                .setRank(0, 'Rank', false)
+
+            rank.build()
+                .then(async (data) => {
+                    const attachment = new Discord.MessageAttachment(data, "RankCard.png");
+                    await interaction.reply({ content: 'Here is your level card!', files: [attachment], ephemeral: true })
+                });
+
+        }
 
     })
 
     s4d.client.on('messageCreate', async (param1) => {
-        // if (param1.content == 'test01') {
-        //     s4d.joiningMember = param1;
-        //     s4d.client.channels.cache.get('1041049828525887568').send({
-        //         content: '** **',
-        //         files: [{
-        //             attachment: (await new canvas.Welcome().setUsername(((s4d.joiningMember.author).username)).setMemberCount(((s4d.joiningMember.guild).memberCount)).setDiscriminator(((s4d.joiningMember.author).discriminator)).setAvatar(((s4d.joiningMember.author).displayAvatarURL({
-        //                 format: "png"
-        //             }))).setGuildName(((s4d.joiningMember.guild).name)).setBackground('https://cdn.discordapp.com/attachments/1041108346398314616/1041122336411230258/unknown.png').setColor("title", "#ffffff").setColor("title-border", "#000000").toAttachment()).toBuffer()
-        //         }]
-        //     })
-        //     s4d.joiningMember = null
-        // }
-
+        await leveling.checkData(param1.author.id, param1.guild.id)
+        if (param1.content == '.test01') {
+            s4d.joiningMember = param1;
+            s4d.client.channels.cache.get('1041049828525887568').send({
+                content: '** **',
+                files: [{
+                    attachment: (await new canvas.Welcome().setUsername(((s4d.joiningMember.author).username)).setMemberCount(((s4d.joiningMember.guild).memberCount)).setDiscriminator(((s4d.joiningMember.author).discriminator)).setAvatar(((s4d.joiningMember.author).displayAvatarURL({
+                        format: "png"
+                    }))).setGuildName(((s4d.joiningMember.guild).name)).setBackground('https://cdn.discordapp.com/attachments/1041372494717784186/1042354696171552788/unknown.png').setColor("title", "#ffffff").setColor("title-border", "#000000").setColor("hashtag", "#ffffff").setColor("hashtag", "#ffffff").toAttachment()).toBuffer()
+                }]
+            });
+            s4d.joiningMember = null
+        }
     });
 
     s4d.client.on('guildMemberAdd', async (param1) => {
@@ -102,7 +140,7 @@
             files: [{
                 attachment: (await new canvas.Welcome().setUsername(((s4d.joiningMember.user).username)).setMemberCount(((s4d.joiningMember.guild).memberCount)).setDiscriminator(((s4d.joiningMember.user).discriminator)).setAvatar(((s4d.joiningMember.user).displayAvatarURL({
                     format: "png"
-                }))).setGuildName(((s4d.joiningMember.guild).name)).setBackground('https://cdn.discordapp.com/attachments/1041108346398314616/1041122336411230258/unknown.png').setColor("title", "#ffffff").setColor("title-border", "#ffffff").toAttachment()).toBuffer()
+                }))).setGuildName(((s4d.joiningMember.guild).name)).setBackground('https://cdn.discordapp.com/attachments/1041372494717784186/1042354696171552788/unknown.png').setColor("title", "#ffffff").setColor("title-border", "#000000").setColor("hashtag", "#000000").setColor("hashtag", "#000000").toAttachment()).toBuffer()
             }]
         });
         s4d.joiningMember = null
